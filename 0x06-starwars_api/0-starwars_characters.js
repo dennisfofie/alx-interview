b4b10763373379a867fb3153e06a4470f6cb4fc2
Dennis Fofie <dfofie@tapnhop.com>
2024-01-16 07:11:32 +0000

    final commit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 const request = require('request');
 
 let movieId = 0;
@@ -6,31 +7,31 @@ if (process.argv.length > 2) {
     movieId = process.argv[2];
   }
   else {
-    throw new Error("MovieId must be passed to CMD")
+    throw new Error("MovieId must be passed to CMD");
   }
 
-  const BASE_ENDPOINT = `https://swapi-api.alx-tools.com/api/films/${movieId}`
-  const CHARACTER_ENDPOINT = "https://swapi-api.alx-tools.com/api/people/"
+  const BASE_ENDPOINT = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+  const CHARACTER_ENDPOINT = "https://swapi-api.alx-tools.com/api/people/";
 
   try {
     request(BASE_ENDPOINT, (error, response, body) => {
         if (error) {
-            throw new Error("Invalid request")
+            throw new Error("Invalid request");
         }
         else if (response.statusCode != 200) {
             throw new Error("Resource is prohibited")
         }
-        const result = JSON.parse(body)
-        console.log(result)
+        const result = JSON.parse(body);
+        console.log(result);
 
         for (let i = 1; i <= result['characters'].length; i++) {
             request(CHARACTER_ENDPOINT + i, (error, response, body) => {
                 if (error) {
-                    console.log(error)
+                    console.log(error);
                 }
                 if (response.statusCode == 200) {
-                    const secondResult = JSON.parse(body)
-                    console.log(secondResult['name'])
+                    const secondResult = JSON.parse(body);
+                    console.log(secondResult['name']);
 
                 }
             })
